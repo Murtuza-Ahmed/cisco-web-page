38442e3b64e2c695e00bc3c5cdbb89309f5251e9
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-09 15:42:42 +0500

    navbar component style complete and not responsive

--- cisco-web-page-design/src/Components/HeadLine/HeadLine.jsx
+++ cisco-web-page-design/src/Components/HeadLine/HeadLine.jsx
@@ -7,35 +7,38 @@ import Logo from "../../assets/image/logo.png";
 
 const Headline = () => {
   return (
-    <div className={styles.Headline}>
-      <div className={styles.Logo}>
-        <img src={Logo} alt="Logo_CiscoCompany" style={{ width: "80px" }} />
+    <div>
+      <div className={styles.Headline}>
+        <div className={styles.Logo}>
+          <img src={Logo} alt="Logo_CiscoCompany" style={{ width: "80px" }} />
+        </div>
+        <div className={styles.Container}>
+          <div className={styles.Contact}>
+            <ul>
+              <li>Contact</li>
+              <li>Partners</li>
+            </ul>
+          </div>
+          <div className={styles.LoginIcon}>
+            <ul>
+              <img src={Login_icon} alt="UserIcon" style={{ width: "20px" }} />
+              <li>Log In</li>
+              <img
+                src={Serach_icon}
+                alt="Search-Icon"
+                style={{ width: "20px" }}
+              />
+            </ul>
+          </div>
+          <div className={styles.Languge}>
+            <ul>
+              <img src={World_icon} alt="WorldIcon" style={{ width: "15px" }} />
+              <li>En US</li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className={styles.Container}>
-        <div className={styles.Contact}>
-          <ul>
-            <li>Contact</li>
-            <li>Partners</li>
-          </ul>
-        </div>
-        <div className={styles.LoginIcon}>
-          <ul>
-            <img src={Login_icon} alt="UserIcon" style={{ width: "20px" }} />
-            <li>Log In</li>
-            <img
-              src={Serach_icon}
-              alt="Search-Icon"
-              style={{ width: "20px" }}
-            />
-          </ul>
-        </div>
-        <div className={styles.Languge}>
-          <ul>
-            <img src={World_icon} alt="WorldIcon" style={{ width: "15px" }} />
-            <li>En US</li>
-          </ul>
-        </div>
-      </div>
+      <hr />
     </div>
   );
 };
