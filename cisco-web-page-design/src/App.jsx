d77c6bd14e5d79548b05bd03dfc0b0edde8f4f47
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-09 14:15:45 +0500

    responsive paractice

--- cisco-web-page-design/src/App.jsx
+++ cisco-web-page-design/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <Headline />
-      <Navbar />
+      {/* <Navbar /> */}
     </div>
   );
 };
--- cisco-web-page-design/src/Components/HeadLine/HeadLine.jsx
+++ cisco-web-page-design/src/Components/HeadLine/HeadLine.jsx
@@ -2,25 +2,39 @@ import React from "react";
 import styles from "./HeadLine.module.css";
 import Login_icon from "../../assets/image/loginicon.png";
 import World_icon from "../../assets/image/worldicon.png";
+import Serach_icon from "../../assets/image/searchicon.png";
+import Logo from "../../assets/image/logo.png";
 
 const Headline = () => {
   return (
     <div className={styles.Headline}>
+      <div className={styles.Logo}>
+        <img src={Logo} alt="Logo_CiscoCompany" style={{ width: "80px" }} />
+      </div>
       <div className={styles.Container}>
-        <ul>
-          <li>Contact</li>
-          <li>Partners</li>
-          <div className={styles.LoginIcon}>
-            <ul>
-              <img src={Login_icon} alt="UserIcon" style={{ width: "20px" }} />
-              <li>Log In</li>
-            </ul>
-          </div>
-          <div className={styles.Languge}>
+        <div className={styles.Contact}>
+          <ul>
+            <li>Contact</li>
+            <li>Partners</li>
+          </ul>
+        </div>
+        <div className={styles.LoginIcon}>
+          <ul>
+            <img src={Login_icon} alt="UserIcon" style={{ width: "20px" }} />
+            <li>Log In</li>
+            <img
+              src={Serach_icon}
+              alt="Search-Icon"
+              style={{ width: "20px" }}
+            />
+          </ul>
+        </div>
+        <div className={styles.Languge}>
+          <ul>
             <img src={World_icon} alt="WorldIcon" style={{ width: "15px" }} />
             <li>En US</li>
-          </div>
-        </ul>
+          </ul>
+        </div>
       </div>
     </div>
   );
