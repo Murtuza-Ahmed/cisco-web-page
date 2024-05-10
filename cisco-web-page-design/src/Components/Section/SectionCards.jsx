43f355b651375438b04fdf6d6692a6500742d20a
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-10 19:38:51 +0500

    paractice

--- cisco-web-page-design/src/Components/Section/SectionCards.jsx
+++ cisco-web-page-design/src/Components/Section/SectionCards.jsx
@@ -10,11 +10,13 @@ const SectionCards = ({
       <div className={styles.SectionCards}>
         <div className={styles.Cards}>
           {<img src={getImageUrl(imageSrc)} style={{ width: "130%" }} />}
-          <h3 className={styles.Title}>{title}</h3>
-          <p className={styles.Description}>{description}</p>
-          <div className={styles.Link}>
-            <a href="#">{source}</a>
-            <img src={getImageUrl(breadcrum)} />
+          <div className={styles.Container}>
+            <h3 className={styles.Title}>{title}</h3>
+            <p className={styles.Description}>{description}</p>
+            <div className={styles.Link}>
+              <a href="#">{source}</a>
+              <img src={getImageUrl(breadcrum)} />
+            </div>
           </div>
         </div>
       </div>
