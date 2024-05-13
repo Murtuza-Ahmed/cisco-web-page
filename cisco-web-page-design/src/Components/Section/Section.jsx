a6d9f407a11e2a5767d3def206522dd719122ed7
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-13 13:58:30 +0500

    section componrnt set

--- cisco-web-page-design/src/Components/Section/Section.jsx
+++ cisco-web-page-design/src/Components/Section/Section.jsx
@@ -6,14 +6,20 @@ import SectionCards from "./SectionCards";
 const Section = () => {
   return (
     <div>
-      <div className={styles.Section}>
-        <div className={styles.Title}>
-          <h1>Latest products and innovations from the RSA Conference</h1>
-        </div>
-        <div className={styles.Products}>
-          {CiscoData.map((item, i) => (
-            <SectionCards key={i} items={item} />
-          ))}
+      <div className={styles.Main}>
+        <div className={styles.Container}>
+          <div className={styles.SectionPadding}>
+            <div className={styles.Section}>
+              <div className={styles.Title}>
+                <h1>Latest products and innovations from the RSA Conference</h1>
+              </div>
+              <div className={styles.Products}>
+                {CiscoData.map((item, i) => (
+                  <SectionCards key={i} items={item} />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
