f1676b532e0924b3adc509ea6368c582d79803fc
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-13 14:39:08 +0500

    project complete and set

--- cisco-web-page-design/src/Components/InsideCisco/InsideCisco.jsx
+++ cisco-web-page-design/src/Components/InsideCisco/InsideCisco.jsx
@@ -7,25 +7,31 @@ import InsideCiscoCards from "./InsideCiscoCards";
 const InsideCisco = () => {
   return (
     <div>
-      <div className={styles.InsideCisco}>
-        <div className={styles.Content}>
-          <h1>Inside Cisco</h1>
-          <div className={styles.IconContainer}>
-            <div className={styles.Icon}>
-              <a href="#">More news</a>
-              <img src={getImageUrl("breadcrum_arrow.png")} />
+      <div className={styles.Main}>
+        <div className={styles.Container}>
+          <div className={styles.InsideCisco_Padding}>
+            <div className={styles.InsideCisco}>
+              <div className={styles.Content}>
+                <h1>Inside Cisco</h1>
+                <div className={styles.IconContainer}>
+                  <div className={styles.Icon}>
+                    <a href="#">More news</a>
+                    <img src={getImageUrl("breadcrum_arrow.png")} />
+                  </div>
+                  <div className={styles.Icon}>
+                    <a href="#">More events</a>
+                    <img src={getImageUrl("breadcrum_arrow.png")} />
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className={styles.Icon}>
-              <a href="#">More events</a>
-              <img src={getImageUrl("breadcrum_arrow.png")} />
+            <div className={styles.InsideCiscoCardsContainer}>
+              {Inside_Cisco.map((item, i) => (
+                <InsideCiscoCards key={i} items={item} />
+              ))}
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.InsideCiscoCardsContainer}>
-        {Inside_Cisco.map((item, i) => (
-          <InsideCiscoCards key={i} items={item} />
-        ))}
       </div>
     </div>
   );
