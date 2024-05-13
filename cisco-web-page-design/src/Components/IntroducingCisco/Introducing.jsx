75fd05bb906539cd39028d3b5dcadaa4da78f520
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-13 14:25:37 +0500

    intro component complete

--- cisco-web-page-design/src/Components/IntroducingCisco/Introducing.jsx
+++ cisco-web-page-design/src/Components/IntroducingCisco/Introducing.jsx
@@ -6,17 +6,23 @@ import Video from "./Video.jsx";
 const Introducing = () => {
   return (
     <div>
-      <div className={styles.Introducing}>
-        <div className={styles.Content}>
-          <h3>Introducing Cisco Hypershield</h3>
-          <p>Unveiling a new era of AI-native security.</p>
-          <div className={styles.Icon}>
-            <a href="#">Explore Cisco Hypershield</a>
-            <img src={getImageUrl("breadcrum_arrow.png")} />
+      <div className={styles.Main}>
+        <div className={styles.Container}>
+          <div className={styles.Introducing_Padding}>
+            <div className={styles.Introducing}>
+              <div className={styles.Content}>
+                <h3>Introducing Cisco Hypershield</h3>
+                <p>Unveiling a new era of AI-native security.</p>
+                <div className={styles.Icon}>
+                  <a href="#">Explore Cisco Hypershield</a>
+                  <img src={getImageUrl("breadcrum_arrow.png")} />
+                </div>
+              </div>
+            </div>
+            <Video />
           </div>
         </div>
       </div>
-      <Video/>
     </div>
   );
 };
