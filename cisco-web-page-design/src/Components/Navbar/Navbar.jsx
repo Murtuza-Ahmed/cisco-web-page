d51736e32abd3ab791330db2398a68977f0a19a5
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-13 12:50:59 +0500

    project set

--- cisco-web-page-design/src/Components/Navbar/Navbar.jsx
+++ cisco-web-page-design/src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,45 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-import Logo from "../../assets/image/logo.png";
+import Logo from "../../assets/image/cisco-logo.svg";
 import Search_icon from "../../assets/image/searchicon.png";
 
 const Navbar = () => {
   return (
-    <div className={styles.Navbar}>
-      <div className={styles.Container}>
-        <div className={styles.Image}>
-          <img src={Logo} alt="Cisco Company Logo" style={{ width: "100px" }} />
-        </div>
-        <div className={styles.Menu}>
-          <ul>
-            <li>Products and Services</li>
-            <li>Solutions</li>
-            <li>Support</li>
-            <li>Learn</li>
-          </ul>
-        </div>
-        <div className={styles.RightMenu}>
-          <ul>
-            <li>Explore Cisco</li>
-            <li>Search</li>
-            <img
-              src={Search_icon}
-              alt="Search-Icon"
-              style={{ width: "20px" }}
-            />
-          </ul>
+    <div>
+      <div className={styles.Main}>
+        <div className={styles.Container}>
+          <div className={styles.NavbarPadding}>
+            <div className={styles.Navbar}>
+              <div className={styles.ContainerSection}>
+                <div className={styles.Image}>
+                  <img
+                    src={Logo}
+                    alt="Cisco Company Logo"
+                    style={{ width: "100px" }}
+                  />
+                </div>
+                <div className={styles.Menu}>
+                  <ul>
+                    <li>Products and Services</li>
+                    <li>Solutions</li>
+                    <li>Support</li>
+                    <li>Learn</li>
+                  </ul>
+                </div>
+                <div className={styles.RightMenu}>
+                  <ul>
+                    <li>Explore Cisco</li>
+                    <li>Search</li>
+                    <img
+                      src={Search_icon}
+                      alt="Search-Icon"
+                      style={{ width: "20px" }}
+                    />
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
