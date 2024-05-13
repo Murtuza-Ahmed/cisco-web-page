d0eaf3bd4be3415bb22ce20354c50b6f7b0d125c
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-05-13 12:06:52 +0500

    project set

--- cisco-web-page-design/src/Components/HeadLine/HeadLine.jsx
+++ cisco-web-page-design/src/Components/HeadLine/HeadLine.jsx
@@ -8,35 +8,53 @@ import Logo from "../../assets/image/logo.png";
 const Headline = () => {
   return (
     <div>
-      <div className={styles.Headline}>
-        <div className={styles.Logo}>
-          <img src={Logo} alt="Logo_CiscoCompany" style={{ width: "80px" }} />
-        </div>
+      <div className={styles.Main}>
         <div className={styles.Container}>
-          <div className={styles.Contact}>
-            <ul>
-              <li>Contact</li>
-              <li>Partners</li>
-            </ul>
-          </div>
-          <div className={styles.LoginIcon}>
-            <ul>
-              <img src={Login_icon} alt="UserIcon" style={{ width: "20px" }} />
-              <li>Log In</li>
-              <div className={styles.SearchIcon}>
+          <div className={styles.HeadlinePadding}>
+            <div className={styles.Headline}>
+              <div className={styles.Logo}>
                 <img
-                  src={Serach_icon}
-                  alt="Search-Icon"
-                  style={{ width: "20px" }}
+                  src={Logo}
+                  alt="Logo_CiscoCompany"
+                  style={{ width: "80px" }}
                 />
               </div>
-            </ul>
-          </div>
-          <div className={styles.Languge}>
-            <ul>
-              <img src={World_icon} alt="WorldIcon" style={{ width: "15px" }} />
-              <li>EN US</li>
-            </ul>
+              <div className={styles.ContainerSection}>
+                <div className={styles.Contact}>
+                  <ul>
+                    <li>Contact</li>
+                    <li>Partners</li>
+                  </ul>
+                </div>
+                <div className={styles.LoginIcon}>
+                  <ul>
+                    <img
+                      src={Login_icon}
+                      alt="UserIcon"
+                      style={{ width: "20px" }}
+                    />
+                    <li>Log In</li>
+                    <div className={styles.SearchIcon}>
+                      <img
+                        src={Serach_icon}
+                        alt="Search-Icon"
+                        style={{ width: "20px" }}
+                      />
+                    </div>
+                  </ul>
+                </div>
+                <div className={styles.Languge}>
+                  <ul>
+                    <img
+                      src={World_icon}
+                      alt="WorldIcon"
+                      style={{ width: "15px" }}
+                    />
+                    <li>EN US</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
