25d90da094dbe6ee20b608debaaf88d5ca38f014
murtuza_ahmed <murtuza.programmer@gmail.com>
2024-06-05 18:17:18 +0500

    responsive problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
-import Content from "./Components/Content/Content";
-import Section from "./Components/Section/Section";
-import SectionA from "./Components/SectionA/SectionA";
-import Introducing from "./Components/IntroducingCisco/Introducing";
-import InsideCisco from "./Components/InsideCisco/InsideCisco";
-import Footer from "./Components/Footer/Footer";
+// import Hero from "./Components/Hero/Hero";
+// import Content from "./Components/Content/Content";
+// import Section from "./Components/Section/Section";
+// import SectionA from "./Components/SectionA/SectionA";
+// import Introducing from "./Components/IntroducingCisco/Introducing";
+// import InsideCisco from "./Components/InsideCisco/InsideCisco";
+// import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <Content />
       <Section />
       <SectionA />
       <Introducing />
       <InsideCisco />
-      <Footer />
+      <Footer /> */}
       
     </>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import styles from "./navbar.module.css";
 import commonStyle from "../../assets/Style/common.module.css";
+import logo from "../../assets/image/cutting-logo.png";
+import searchIcon from "../../assets/image/searchicon.png";
 
 const Navbar = () => {
   return (
@@ -11,9 +13,40 @@ const Navbar = () => {
           >
             <div className={styles.nav_padding}>
               <div className={styles.row}>
-                <div className={styles.column}></div>
-                <div className={styles.column}></div>
-                <div className={styles.column}></div>
+                <div className={styles.column_10}>
+                  <div className={styles.logo}>
+                    <img src={logo} alt="Logo" />
+                  </div>
+                  <div className={styles.column_responive}>
+                    <ul>
+                      <li>Login</li>
+                      <li>
+                        <img src={searchIcon} alt="Search_Icon" />
+                      </li>
+                      <li></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className={styles.column_50}>
+                  <div className={styles.left_ul}>
+                    <ul>
+                      <li>Products & Services</li>
+                      <li>Solution</li>
+                      <li>Support</li>
+                      <li>Learn</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className={styles.column_30}>
+                  <div className={styles.right_ul}>
+                    <ul>
+                      <li>Explore Cisco</li>
+                      <li>
+                        Search <img src={searchIcon} alt="Search_Icon" />
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
